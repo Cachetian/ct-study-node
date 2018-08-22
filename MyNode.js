d10b5346fd5d042ab42aebcb3a46b662476ace08
tianmu <hit.mutian@qq.com>
2018-08-22 21:47:29 +0800

    V6

--- MyNode.js
+++ MyNode.js
@@ -36,16 +36,18 @@
             res.write("<!DOCTYPE html>" +
               "<html>" +
               "<head>" +
+              "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" +
               "<meta charset=\"utf-8\">" +
-              "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" +
               "<title>My Node</title>" +
-              "<script src=\"https://code.jquery.com/jquery-3.3.1.min.js\" integrity=\"sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=\" crossorigin=\"anonymous\"></script>" +
-              "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\"></script>" +
-              "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">" +
+              "<script " +
+              "id=\"sap-ui-bootstrap\"" +
+              "src=\"https://openui5.hana.ondemand.com/resources/sap-ui-core.js\"" +
+              "data-sap-ui-theme=\"sap_belize\"" +
+              "data-sap-ui-libs=\"sap.m\"" +
+              "data-sap-ui-preload=\"async\">" +
+              "</script>" +
               "</head>" +
-              "<body>" +
-              "<div class=\"container\">" +
-              "</div>" +
+              "<body class=\"sapUiBody\" id=\"content\">" +
               "</body>" +
               "<script src=\"/MyNode.js\"></script>" +
               "</html>");
@@ -75,24 +77,40 @@
     console.log("Client start");
     $(document).ready(function() {
       console.log("Client ready");
-      var element = document.getElementsByClassName("container")[0];
+      sap.ui.getCore().attachInit(function() {
+        // create a mobile app and display page1 initially
+        var app = new sap.m.App("myApp", {
+          initialPage: "page1"
+        });
 
-      var head = document.createElement("h1");
-      var headNode = document.createTextNode("My First Bootstrap Page.");
-      head.appendChild(headNode);
-      element.appendChild(head);
+        // create the first page
+        var page1 = new sap.m.Page("page1", {
+          title: "Hello World",
+          showNavButton: false,
+          content: new sap.m.Button({
+            text: "Go to Page 2",
+            press: function() {
+              // navigate to page2
+              app.to("page2");
+            }
+          })
+        });
 
-      var para = document.createElement("p");
-      var paraNode = document.createTextNode("This is some text.");
-      para.appendChild(paraNode);
-      element.appendChild(para);
+        // create the second page with a back button
+        var page2 = new sap.m.Page("page2", {
+          title: "Hello Page 2",
+          showNavButton: true,
+          navButtonPress: function() {
+            // go back to the previous page
+            app.back();
+          }
+        });
 
-      var button = document.createElement("button");
-      button.className = "btn";
-      button.type = "button";
-      var buttonNode = document.createTextNode("click");
-      button.appendChild(buttonNode);
-      element.appendChild(button);
+        // add both pages to the app
+        app.addPage(page1).addPage(page2);
+        // place the app into the HTML document
+        app.placeAt("content");
+      });
     });
 
   }
