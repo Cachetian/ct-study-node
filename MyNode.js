bb69e83c08cef330a184a0d411e9dbb17707c18e
tianmu <hit.mutian@qq.com>
2018-11-05 08:44:32 +0800

    [Feature] add proxy server feature
    1. add comments
    2. add http-proxy feature
    3. refact ui5 admin UI code structure.

--- MyNode.js
+++ MyNode.js
@@ -7,29 +7,43 @@
  */
 (function() {
   "use strict";
-  MyNode();
+
+  // run it!
+  return MyNode();
 
   function MyNode() {
+    // if window object found, means in Broswer environment
     if (typeof window === 'undefined') {
       MyServer();
     } else {
       MyBrower();
     }
 
+    /**
+     * Server side
+     */
     function MyServer() {
       console.log("Server start");
-      var sqlite3 = require('sqlite3');
-      var http = require('http');
-      var url = require('url');
-      var fs = require('fs');
-      var mime = require('mime-types');
+      var sqlite3 = require('sqlite3'),
+        http = require('http'),
+        httpProxy = require('http-proxy'),
+        url = require('url'),
+        fs = require('fs'),
+        mime = require('mime-types');
       var db = new sqlite3.Database(':memory:', function() {
         db.run("CREATE TABLE IF NOT EXISTS MY_NODE (id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(36), value varchar(255))", function() {
-          var port = 8080;
+          var port = 8086;
+          var hproxy = httpProxy.createProxyServer();
           http.createServer(function(req, res) {
             console.log(req.url);
             var q = url.parse(req.url, true);
             // APIs
+            // Mock API
+            if (q.pathname === '/sap/bc/lrep/flex/data/huawei.cbs.zcbsder001role.Component') {
+              var lrepRes = '';
+              res.write(lrepRes);
+              return res.end();
+            }
             if (q.pathname === "/shutdown") {
               console.log("process exit!");
               process.exit();
@@ -66,7 +80,7 @@
               });
               return;
             }
-            // web server
+            // Admin UI
             if (q.pathname === "/") {
               res.write("<!DOCTYPE html>" +
                 "<html>" +
@@ -88,7 +102,69 @@
                 "</html>");
               return res.end();
             }
+            // Proxy v2
+            var proxy_cfg = {
+              // the prefix you use to call your backend functions via the proxy server
+              prefix: "/LOC/",
+              // the host of your backend server
+              host: "localhost",
+              // port of your backend server
+              port: "8088"
+            };
+            if (q.pathname.startsWith(proxy_cfg.prefix)) {
+              hproxy.on('error', function(err, preq, pres) {
+                console.log("backend error");
+                console.log(err);
+              });
+              hproxy.on('proxyRes', function(proxyRes, preq, pres) {
+                console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+              });
+              hproxy.on('close', function(preq, socket, head) {
+                // view disconnected websocket connections
+                console.log('Client disconnected');
+              });
+
+              // We have to set the host of the request to the our remote server
+              // It currently contains localhost... which leads to problems on some
+              // servers
+              req.headers.host = proxy_cfg.host;
+              // cut the prefix from the beginning of the url
+              // request.url = request.url.slice(request.url.indexOf("/", 1));
+              req.url = req.url.slice(proxy_cfg.prefix.length);
+              hproxy.web(req, res, {
+                // cause we use this script only during development and testing we
+                // have a http connection. For https we have to do some additional
+                // proxy configuration
+                target: 'http://' + proxy_cfg.host + (proxy_cfg.port ? ':' + proxy_cfg.port : '') + '/'
+              });
+              return;
+            }
+            // Prox v1 has content type incorrect issue
+            // var PROXY_ROOT = "/LOC";
+            // if (q.pathname.startsWith(PROXY_ROOT)) {
+            //   var options = {
+            //     hostname: 'localhost',
+            //     port: 8088,
+            //     path: req.url.substr(PROXY_ROOT.length),
+            //     method: req.method
+            //   };
+            //   var proxy = http.request(options, function (pres) {
+            //     pres.pipe(res, {
+            //       end: true
+            //     });
+            //   });
+            //   req.pipe(proxy, {
+            //     end: true
+            //   });
+            //   return;
+            // }
+            // web server
             var filename = "." + q.pathname;
+            // UI5
+            var UI5_RESOURCE_ROOT = "C:/SAP/WebIDE/sap-webide-personal-edition-1.53.5-trial-win32.win32.x86_64/eclipse/plugins/com.sap.webide.orionplugin_1.53.5/ui5/1.52.18/";
+            if (q.pathname.startsWith("/resources")) {
+              filename = UI5_RESOURCE_ROOT + filename;
+            }
             fs.readFile(filename, function(err, data) {
               if (err) {
                 res.writeHead(404, {
@@ -108,44 +184,49 @@
       });
     }
 
+    /**
+     * Broswer side
+     */
     function MyBrower() {
       console.log("Client start");
       $(document).ready(function() {
         console.log("Client ready");
         sap.ui.getCore().attachInit(function() {
-          var myApp = new sap.m.App("myApp");
-          var myPage = new sap.m.Page("myPage", {
-            title: "Home",
-            showNavButton: false,
-            content: [
-              new sap.m.Button({
-                text: "Hello",
-                press: function() {
-                  jQuery.get("/api/hello", function(data) {
-                    sap.m.MessageToast.show(data);
-                  });
-                }
-              }), new sap.m.Button({
-                text: "Create",
-                press: function() {
-                  jQuery.post("/api/create", JSON.stringify({
-                    "name": "myName",
-                    "value": "myValue"
-                  }), function(res) {
-                    sap.m.MessageToast.show(res);
-                  });
-                }
-              }), new sap.m.Button({
-                text: "Get",
-                press: function() {
-                  jQuery.get("/api/get", function(res) {
-                    sap.m.MessageToast.show(res);
-                  });
-                }
+          var myApp = new sap.m.App("myApp", {
+            pages: [
+              new sap.m.Page("myPage", {
+                title: "Home",
+                showNavButton: false,
+                content: [
+                  new sap.m.Button({
+                    text: "Hello",
+                    press: function() {
+                      jQuery.get("/api/hello", function(data) {
+                        sap.m.MessageToast.show(data);
+                      });
+                    }
+                  }), new sap.m.Button({
+                    text: "Create",
+                    press: function() {
+                      jQuery.post("/api/create", JSON.stringify({
+                        "name": "myName",
+                        "value": "myValue"
+                      }), function(res) {
+                        sap.m.MessageToast.show(res);
+                      });
+                    }
+                  }), new sap.m.Button({
+                    text: "Get",
+                    press: function() {
+                      jQuery.get("/api/get", function(res) {
+                        sap.m.MessageToast.show(res);
+                      });
+                    }
+                  })
+                ]
               })
             ]
           });
-          myApp.addPage(myPage);
           myApp.placeAt("content");
         });
       });
