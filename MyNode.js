d773726a868f1c7f33d8b8207f2fc2766ae2d40b
Sky <1989mutian@163.com>
2019-01-19 01:09:58 +0800

    Update MyNode.js
    Refine code to simplified and enhanced reverse proxy to enable array of prefixes

--- MyNode.js
+++ MyNode.js
@@ -37,13 +37,7 @@
           http.createServer(function(req, res) {
             console.log(req.url);
             var q = url.parse(req.url, true);
-            // APIs
-            // Mock API
-            if (q.pathname === '/sap/bc/lrep/flex/data/huawei.cbs.zcbsder001role.Component') {
-              var lrepRes = '';
-              res.write(lrepRes);
-              return res.end();
-            }
+            // REST APIs
             if (q.pathname === "/shutdown") {
               console.log("process exit!");
               process.exit();
@@ -68,7 +62,6 @@
                   }));
                   res.end();
                 });
-
               });
               return;
             }
@@ -80,91 +73,46 @@
               });
               return;
             }
-            // Admin UI
+            // Index.html
             if (q.pathname === "/") {
-              res.write("<!DOCTYPE html>" +
-                "<html>" +
-                "<head>" +
-                "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" +
-                "<meta charset=\"utf-8\">" +
-                "<title>My Node</title>" +
-                "<script " +
-                "id=\"sap-ui-bootstrap\"" +
-                "src=\"https://openui5.hana.ondemand.com/resources/sap-ui-core.js\"" +
-                "data-sap-ui-theme=\"sap_belize\"" +
-                "data-sap-ui-libs=\"sap.m\"" +
-                "data-sap-ui-preload=\"async\">" +
-                "</script>" +
-                "</head>" +
-                "<body class=\"sapUiBody\" id=\"content\">" +
-                "</body>" +
-                "<script src=\"/MyNode.js\"></script>" +
-                "</html>");
+              res.write(Buffer.from("PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgaHR0cC1lcXVpdj0iWC1VQS1Db21wYXRpYmxlIiBjb250ZW50PSJJRT1lZGdlIj48bWV0YSBjaGFyc2V0PSJ1dGYtOCI+PHRpdGxlPk15IE5vZGU8L3RpdGxlPjxzY3JpcHQgaWQ9InNhcC11aS1ib290c3RyYXAic3JjPSJodHRwczovL29wZW51aTUuaGFuYS5vbmRlbWFuZC5jb20vcmVzb3VyY2VzL3NhcC11aS1jb3JlLmpzImRhdGEtc2FwLXVpLXRoZW1lPSJzYXBfYmVsaXplImRhdGEtc2FwLXVpLWxpYnM9InNhcC5tImRhdGEtc2FwLXVpLXByZWxvYWQ9ImFzeW5jIj48L3NjcmlwdD48L2hlYWQ+PGJvZHkgY2xhc3M9InNhcFVpQm9keSIgaWQ9ImNvbnRlbnQiPjwvYm9keT48c2NyaXB0IHNyYz0iL015Tm9kZS5qcyI+PC9zY3JpcHQ+PC9odG1sPg==", 'base64').toString());
               return res.end();
             }
-            // Proxy v2
-            var proxy_cfg = {
-              // the prefix you use to call your backend functions via the proxy server
-              prefix: "/LOC/",
-              // the host of your backend server
-              host: "localhost",
-              // port of your backend server
-              port: "8088"
-            };
-            if (q.pathname.startsWith(proxy_cfg.prefix)) {
-              hproxy.on('error', function(err, preq, pres) {
-                console.log("backend error");
-                console.log(err);
-              });
-              hproxy.on('proxyRes', function(proxyRes, preq, pres) {
-                console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
-              });
-              hproxy.on('close', function(preq, socket, head) {
-                // view disconnected websocket connections
-                console.log('Client disconnected');
-              });
-
-              // We have to set the host of the request to the our remote server
-              // It currently contains localhost... which leads to problems on some
-              // servers
-              req.headers.host = proxy_cfg.host;
-              // cut the prefix from the beginning of the url
-              // request.url = request.url.slice(request.url.indexOf("/", 1));
-              req.url = req.url.slice(proxy_cfg.prefix.length);
-              hproxy.web(req, res, {
-                // cause we use this script only during development and testing we
-                // have a http connection. For https we have to do some additional
-                // proxy configuration
-                target: 'http://' + proxy_cfg.host + (proxy_cfg.port ? ':' + proxy_cfg.port : '') + '/'
-              });
-              return;
+            // Reverse proxy
+            var aPrefixes = [
+              "/proxy/"
+            ]
+            for (var iPrefix in aPrefixes) {
+              var proxy_cfg = {
+                // the prefix you use to call your backend functions via the proxy server
+                prefix: aPrefixes[iPrefix],
+                // the host of your backend server
+                host: "proxy",
+                // port of your backend server
+                port: "8080"
+              };
+              if (q.pathname.startsWith(proxy_cfg.prefix)) {
+                hproxy.on('error', function(err, preq, pres) {
+                  console.log("backend error");
+                  console.log(err);
+                });
+                hproxy.on('proxyRes', function(proxyRes, preq, pres) {
+                  console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+                });
+                hproxy.on('close', function(preq, socket, head) {
+                  // view disconnected websocket connections
+                  console.log('Client disconnected');
+                });
+                req.headers.host = proxy_cfg.host;
+                req.url = req.url.slice(proxy_cfg.prefix.length);
+                hproxy.web(req, res, {
+                  target: 'http://' + proxy_cfg.host + (proxy_cfg.port ? ':' + proxy_cfg.port : '') + '/'
+                });
+                return;
+              }
             }
-            // Prox v1 has content type incorrect issue
-            // var PROXY_ROOT = "/LOC";
-            // if (q.pathname.startsWith(PROXY_ROOT)) {
-            //   var options = {
-            //     hostname: 'localhost',
-            //     port: 8088,
-            //     path: req.url.substr(PROXY_ROOT.length),
-            //     method: req.method
-            //   };
-            //   var proxy = http.request(options, function (pres) {
-            //     pres.pipe(res, {
-            //       end: true
-            //     });
-            //   });
-            //   req.pipe(proxy, {
-            //     end: true
-            //   });
-            //   return;
-            // }
-            // web server
+            // Web server
             var filename = "." + q.pathname;
-            // UI5
-            var UI5_RESOURCE_ROOT = "C:/SAP/WebIDE/sap-webide-personal-edition-1.53.5-trial-win32.win32.x86_64/eclipse/plugins/com.sap.webide.orionplugin_1.53.5/ui5/1.52.18/";
-            if (q.pathname.startsWith("/resources")) {
-              filename = UI5_RESOURCE_ROOT + filename;
-            }
             fs.readFile(filename, function(err, data) {
               if (err) {
                 res.writeHead(404, {
@@ -190,40 +138,48 @@
     function MyBrower() {
       console.log("Client start");
       $(document).ready(function() {
-        console.log("Client ready");
         sap.ui.getCore().attachInit(function() {
+          // Model
+          var oViewModel = new sap.ui.model.json.JSONModel({});
+          sap.ui.getCore().setModel(oViewModel, "viewModel");
+          // View
           var myApp = new sap.m.App("myApp", {
             pages: [
               new sap.m.Page("myPage", {
-                title: "Home",
+                title: "MyNode.js",
+                showHeader: true,
                 showNavButton: false,
-                content: [
-                  new sap.m.Button({
-                    text: "Hello",
-                    press: function() {
-                      jQuery.get("/api/hello", function(data) {
-                        sap.m.MessageToast.show(data);
-                      });
-                    }
-                  }), new sap.m.Button({
-                    text: "Create",
-                    press: function() {
-                      jQuery.post("/api/create", JSON.stringify({
-                        "name": "myName",
-                        "value": "myValue"
-                      }), function(res) {
-                        sap.m.MessageToast.show(res);
-                      });
-                    }
-                  }), new sap.m.Button({
-                    text: "Get",
-                    press: function() {
-                      jQuery.get("/api/get", function(res) {
-                        sap.m.MessageToast.show(res);
-                      });
-                    }
-                  })
-                ]
+                content: [],
+                footer: new sap.m.OverflowToolbar({
+                  content: [
+                    new sap.m.ToolbarSpacer(),
+                    new sap.m.Button({
+                      text: "Hello",
+                      press: function() {
+                        jQuery.get("/api/hello", function(data) {
+                          sap.m.MessageToast.show(data);
+                        });
+                      }
+                    }), new sap.m.Button({
+                      text: "Create",
+                      press: function() {
+                        jQuery.post("/api/create", JSON.stringify({
+                          "name": "myName",
+                          "value": "myValue"
+                        }), function(res) {
+                          sap.m.MessageToast.show(res);
+                        });
+                      }
+                    }), new sap.m.Button({
+                      text: "Get",
+                      press: function() {
+                        jQuery.get("/api/get", function(res) {
+                          sap.m.MessageToast.show(res);
+                        });
+                      }
+                    })
+                  ]
+                })
               })
             ]
           });
